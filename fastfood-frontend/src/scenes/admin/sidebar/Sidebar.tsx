e65a859afd77472a19657143e86dff700d28cda3
Phuong Nguyen <phuongsover2@gmail.com>
2023-07-28 15:28:14 +0700

    add Link router

--- fastfood-frontend/src/scenes/admin/sidebar/Sidebar.tsx
+++ fastfood-frontend/src/scenes/admin/sidebar/Sidebar.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 const Sidebar = () => {
   return (
     <>
@@ -156,20 +157,20 @@ const Sidebar = () => {
                   </button>
                   <ul id="dropdown-crud" className="space-y-2 py-2">
                     <li>
-                      <a
-                        href=""
+                      <Link
+                        to={'/admin/categories'}
+                        className="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700"
+                      >
+                        Categories
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to={'/admin/products'}
                         className="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700"
                       >
                         Products
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href=""
-                        className="text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700"
-                      >
-                        Users
-                      </a>
+                      </Link>
                     </li>
                   </ul>
                 </li>
