06756e2cb3a81c65681eda06b22474456a2b0802
Phuong Nguyen <phuongsover2@gmail.com>
2025-10-15 21:46:02 +0700

    refactor: move CORS configuration to WebConfig and update allowed origins in application.properties

--- fastfood/src/main/java/com/thuctap/fastfood/configurations/SecurityConfig.java
+++ fastfood/src/main/java/com/thuctap/fastfood/configurations/SecurityConfig.java
@@ -1,29 +1,18 @@
 package com.thuctap.fastfood.configurations;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
-  @Value("${allowed-origins}")
-  private String allowedOrigins;
-
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-    http.cors(Customizer.withDefaults());
-
+    http.cors(Customizer.withDefaults()); // Use the CORS configuration from WebConfig
     http.csrf(AbstractHttpConfigurer::disable);
 
     return http.build();
